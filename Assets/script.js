b58b329f5d4c662658e20ab7d722d45c9e5423f8
juanlajara <juanlajara001@gmail.com>
2020-05-10 12:07:48 -0500

    Pseudo coded Steps

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,38 @@
-var counter = document.querySelector("#counter");
-var addButton = document.querySelector("#add");
-var subtractButton = document.querySelector("#subtract");
+/* 
+When User Clicks Start Button
+ a- Start the timer
+ b- Present the 1st question
+      List of the options to question 1
+         1a-Onclick for the right answer - Next Question()
+         1b-Onclick wrong answer - SubtractTime()
+ c-Next Question Repeat b()
+ d-Quiz Ends when Timer = 0 or All Questions are Answered
+ e-present user the option to add Initials with current score
 
-var count = 0;
-counter.textContent = count;
-if (localStorage.getItem("count")) {
-  count = localStorage.getItem("count");
-  counter.textContent = count;
-}
 
-addButton.addEventListener("click", function () {
-  count++;
-  counter.textContent = count;
+*/
 
-  localStorage.setItem("count", count);
-});
+// var counter = document.querySelector("#counter");
+// var addButton = document.querySelector("#add");
+// var subtractButton = document.querySelector("#subtract");
 
-subtractButton.addEventListener("click", function () {
-  count--;
-  counter.textContent = count;
+// var count = 0;
+// counter.textContent = count;
+// if (localStorage.getItem("count")) {
+//   count = localStorage.getItem("count");
+//   counter.textContent = count;
+// }
 
-  localStorage.setItem("count", count);
-});
+// addButton.addEventListener("click", function () {
+//   count++;
+//   counter.textContent = count;
+
+//   localStorage.setItem("count", count);
+// });
+
+// subtractButton.addEventListener("click", function () {
+//   count--;
+//   counter.textContent = count;
+
+//   localStorage.setItem("count", count);
+// });
